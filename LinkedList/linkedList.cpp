2225431008ff0d2f72647252dddb36a84485ffe4
Pawan Nishad <anshpn8@gmail.com>
2024-03-31 01:34:20 +0530

    method update

--- LinkedList/linkedList.cpp
+++ LinkedList/linkedList.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-class LinkedList{
+
     class Node
     {
     private:
@@ -33,6 +33,17 @@ class LinkedList{
         temp->next=newNode;
     }
 
+    void insertAtHead(Node* &head,int data){
+        Node* newNode=new Node(data);
+
+        if(head==NULL){
+            head=newNode;
+            return;
+        }
+        newNode->next=head;
+        head=newNode;
+    }
+
     void display(Node* head){
         Node* temp=head;
         while (temp != NULL)
@@ -40,15 +51,40 @@ class LinkedList{
             cout<<temp->data<<"->";
             temp=temp->next;
         }
-        cout<<"NULL";
+        cout<<"NULL"<<endl;
         
     }
+
+
+        bool search(Node* head,int key){
+            Node* temp=head;
+            while(temp!=NULL){
+                if(temp->data==key){
+                    return true;
+                }
+                temp=temp->next;
+            }
+            return false;
+        }
     int main()
     {
-        Node* head=new Node(1);
-        display(head);
+        Node* head=NULL;
+        insertAtTail(head,1);
+        insertAtTail(head,2);
+        insertAtTail(head,3);
+        insertAtTail(head,4);
+        insertAtHead(head,0);
 
+        
+
+        display(head);
+        //cout<<"hi my name is pawan nishad.";
+        
+
+        if(search(head,4)){
+            cout<<"Key is found."<<endl;
+        }
         return 0;
     }
     
-};
+    
