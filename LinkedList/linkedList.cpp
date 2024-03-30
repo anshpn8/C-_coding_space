6862730f88fdfd98f5d43e1fff0d96b90a0c31b4
Pawan Nishad <anshpn8@gmail.com>
2024-03-31 02:28:42 +0530

    method update

--- LinkedList/linkedList.cpp
+++ LinkedList/linkedList.cpp
@@ -55,6 +55,21 @@ using namespace std;
         
     }
 
+    void deletion(Node* &head,int value){
+        Node* temp=head;
+        if(head->data==value){
+            Node* toDelete=head;
+            head=head->next;
+            delete toDelete;
+            return;
+        }
+        while(temp->next->data != value){
+            temp=temp->next;
+        }
+        Node* toDelete=temp->next;
+        temp->next=temp->next->next;
+        delete toDelete;
+    }
 
         bool search(Node* head,int key){
             Node* temp=head;
@@ -66,24 +81,55 @@ using namespace std;
             }
             return false;
         }
+
+        Node* reverse(Node* head){
+            Node* prevptr=NULL;
+            Node* currptr=head;
+            Node* nextptr;
+            while(currptr!=NULL){
+                nextptr=currptr->next;
+                currptr->next=prevptr;
+
+                prevptr=currptr;
+                currptr=nextptr;
+            }
+            return prevptr;
+        }
+    Node* reverseRecursive(Node* head){
+        if(head==NULL || head->next==NULL){
+            return head;
+        }
+        Node* newhead=reverseRecursive(head->next);
+        head->next->next=head;
+        head->next=NULL;
+
+        return newhead;
+    }
     int main()
     {
         Node* head=NULL;
+        //insert the value
         insertAtTail(head,1);
         insertAtTail(head,2);
         insertAtTail(head,3);
         insertAtTail(head,4);
         insertAtHead(head,0);
+        insertAtTail(head,5);
+        insertAtTail(head,6);
 
         
-
-        display(head);
-        //cout<<"hi my name is pawan nishad.";
+        
         
 
+        // to search 4
         if(search(head,4)){
             cout<<"Key is found."<<endl;
         }
+        //delete the value from linked list
+        deletion(head,0);
+        
+        //display the data of link list
+        display(head=reverse(head));
         return 0;
     }
     
